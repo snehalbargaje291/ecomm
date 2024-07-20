e6c6ca3bc67f3d7a73ad2975aab00f00eb766069
snehalbargaje291 <snehal.bargaje291@gmail.com>
2024-07-20 23:40:15 +0530

    :bug:

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -3,15 +3,15 @@ import React from "react";
 const Error = () => {
   return (
     <div>
-      <div class="pt-20 bg-slate-900">
-        <div class="flex justify-center items-center flex-col">
+      <div className="pt-20 bg-slate-900">
+        <div className="flex justify-center items-center flex-col">
           <img src="public/not-found.png" alt="" className="h-32 w-32" />
 
-          <h1 class="mt-6 text-2xl font-bold tracking-tight text-gray-300 sm:text-4xl">
+          <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-300 sm:text-4xl">
             Uh-oh!
           </h1>
 
-          <p class="mt-4 text-gray-500 dark:text-gray-400">
+          <p className="mt-4 text-gray-500 dark:text-gray-400">
             We can't find that page.
           </p>
         </div>
