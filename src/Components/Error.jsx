6ae0139e3633f2f26c01ea279b39a04459616b74
snehalbargaje291 <snehal.bargaje291@gmail.com>
2024-07-20 23:42:18 +0530

    :bug:

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -5,7 +5,7 @@ const Error = () => {
     <div>
       <div className="pt-20 bg-slate-900">
         <div className="flex justify-center items-center flex-col">
-          <img src="public/not-found.png" alt="" className="h-32 w-32" />
+          <img src="/public/not-found.png" alt="" className="h-32 w-32" />
 
           <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-300 sm:text-4xl">
             Uh-oh!
